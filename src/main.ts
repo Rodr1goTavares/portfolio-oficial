ce743d62ecd49d21773019f3e0a84a9e198f8497
Rodrigo Tavares <86687715+Rodr1goTavares@users.noreply.github.com>
2023-03-15 16:25:50 -0300

    finalizado

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { SendEmail } from "./modules/sendEmail";
 import { Tech_info } from "./modules/techInfo";
 
 
@@ -5,6 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //funcionalidades
     Tech_info.show();
+    SendEmail.send();
+
+    //footer
+    const dataAtual = new Date().getFullYear();
+    const data = document.getElementById('date') as HTMLElement;
+    data.innerHTML = `${dataAtual}`;
 })
   
 
--- src/modules/sendEmail.ts
+++ src/modules/sendEmail.ts
@@ -1,3 +1,13 @@
 export class SendEmail{
-    
+    public static send(): void{
+        const bt = (<HTMLButtonElement>document.getElementById('confirm-email'));
+        bt.addEventListener('click', () => {
+            const name = (<HTMLInputElement>document.getElementById('name')).value;
+            const email = (<HTMLInputElement>document.getElementById('email')).value;
+            const message = (<HTMLInputElement>document.getElementById('message')).value;
+
+            const emailBody = `Nome: ${name}\nE-mail: ${email}\nMensagem: ${message}`;
+            window.location.href = `mailto:rodr1gowzxz2@gmail.com?subject=Assunto do e-mail&body=${encodeURIComponent(emailBody)}`;
+        })
+    }
 }
