85b5cc353987cdcd03395ff293a53d01ae6fcd48
Rodrigo Tavares <86687715+Rodr1goTavares@users.noreply.github.com>
2023-03-18 10:53:21 -0300

    .

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -5,13 +5,3 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
   });
 
-
-//Modal
-// const myModal = document.getElementById('myModal')
-// const myInput = document.getElementById('myInput')
-
-// myModal.addEventListener('shown.bs.modal', () => {
-//   myInput.focus()
-// })
-
-/*--------------------<Bootstrap>--------------------*/
